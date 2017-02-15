d5abd41e2b41563240451253a81f3ec6f0e6bb4f
Mohsen ThinkPad <mohsen.mirhassani@gmail.com>
2017-02-15 16:35:38 +0100

    new oa client

--- src/rover_action_oa_client.cpp
+++ src/rover_action_oa_client.cpp
@@ -2,11 +2,20 @@
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
 #include <rover_actions/DriveToOAAction.h>
+geometry_msgs::PoseStamped goal_pose;
+bool new_goal = false;
+void goal_cb(const geometry_msgs::PoseStamped::ConstPtr msg)
+{
+  goal_pose = *msg;
+  ROS_INFO("Goal received Going there :) Hope to not Fuck up :D!");
+  new_goal = true;
+}
 
 int main (int argc, char **argv)
 {
   ros::init(argc, argv, "test_DriveToOA");
-
+  ros::NodeHandle nh;
+  ros::Subscriber goal_sub = nh.subscribe("/move_base_simple/goal",1,goal_cb);
   // create the action client
   // true causes the client to spin its own thread
   actionlib::SimpleActionClient<rover_actions::DriveToOAAction> ac("DriveToOA", true);
@@ -18,23 +27,25 @@ int main (int argc, char **argv)
   ROS_INFO("Action server started, sending goal.");
   // send a goal to the action
   rover_actions::DriveToOAGoal goal;
-  goal.goal_pose.position.x = 2.0;
-  goal.goal_pose.position.y = 2.0;
-  goal.goal_pose.position.z = 0.0;
-  goal.goal_pose.orientation.w = 1.0;
-  ac.sendGoal(goal);
-
-  //wait for the action to return
-  bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
-
-  if (finished_before_timeout)
+  ros::Rate r(10);
+  while(ros::ok())
   {
-    actionlib::SimpleClientGoalState state = ac.getState();
-    ROS_INFO("Action finished: %s",state.toString().c_str());
-  }
-  else
-    ROS_INFO("Action did not finish before the time out.");
+    if (new_goal) {
+      new_goal = false;
+      goal.goal_pose = goal_pose.pose;
+      ac.sendGoal(goal);
 
-  //exit
-  return 0;
+      //wait for the action to return
+      bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+
+      if (finished_before_timeout)
+      {
+        actionlib::SimpleClientGoalState state = ac.getState();
+        ROS_INFO("Action finished: %s",state.toString().c_str());
+      }
+      else
+        ROS_INFO("Action did not finish before the time out.");
+    }
+    r.sleep();
+  }
 }
